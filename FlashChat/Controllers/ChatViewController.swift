a15ae241d42d50265a1d42ac1ccc240505fde671
GiuAte <giulioaterno@gmail.com>
2023-04-17 14:45:13 +0200

    First commit

---
+++ FlashChat/Controllers/ChatViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ChatViewController.swift
+//  FlashChat
+//
+//  Created by Giulio Aterno on 17/04/23.
+//
+
+import UIKit
+
+class ChatViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var messageTextfield: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func sendPressed(_ sender: UIButton) {
+    }
+    
+
+}
---
+++ FlashChat/Controllers/LoginViewController.swift
@@ -0,0 +1,19 @@
+//
+//  LoginViewController.swift
+//  FlashChat
+//
+//  Created by Giulio Aterno on 17/04/23.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var emailTextfield: UITextField!
+    @IBOutlet weak var passwordTextfield: UITextField!
+    
+
+    @IBAction func loginPressed(_ sender: UIButton) {
+    }
+    
+}
